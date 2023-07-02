94b41314be480154c687aeeac37b1456c4455cf8
ahmed <ahmed.michrgui@hotmail.fr>
2023-07-02 17:38:17 +0100

    template inegrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import {RouterLinkWithHref} from "@angular/router";
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SidebarComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterLinkWithHref
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
